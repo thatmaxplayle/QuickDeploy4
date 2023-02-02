64f22b69f7a8f3b328447727a46348feb439b0fb
Max Playle <maxplayle04@gmail.com>
2023-02-02 11:39:21 +0000

    Add `EnsureFile` deployment type

--- QuickDeploy4/Data/Enum/EDeploymentType.cs
+++ QuickDeploy4/Data/Enum/EDeploymentType.cs
@@ -8,7 +8,19 @@ namespace QuickDeploy4.Data.Enum
 {
     public enum EDeploymentType
     {
+        /// <summary>
+        /// A file within the source directory, which will be copied over to the destination directory.
+        /// </summary>
         File,
-        Folder
+
+        /// <summary>
+        /// A folder within the source directory, which will be copied (whether this is copied recursively depends on the deployment) over to the destination directory.
+        /// </summary>
+        Folder,
+
+        /// <summary>
+        /// An absolute file path which must exist before the deployment can be executed.
+        /// </summary>
+        EnsureFile
     }
 }
